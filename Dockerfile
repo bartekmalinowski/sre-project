35d3109ffa81a3ace5f3deb48c2d1aee9e7b6c9d
bartekmalinowski <bm306631@student.polsl.pl>
2025-07-19 14:25:13 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Lekki obraz w python
+FROM python:3.11-slim-buster
+
+#enviroment config 
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8000
+
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "app:app"]
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World!"
+
+@app.route('/status')
+def status():
+    return jsonify ({"status": "OK", "service": "SRE Project App"})
+
+@app.route('/error')
+def error():
+    response = make_response(jsonify({"error": "Internal Server Error"}), 500)
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
