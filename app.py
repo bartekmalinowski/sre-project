35f216ec1e71fbadd31a913e32a06a371197662e
bartekmalinowski <bm306631@student.polsl.pl>
2025-07-20 11:33:40 +0200

    Formatted code

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return "Hello, World! Welcome to the SRE Project App."
 
-@app.route('/status')
-def status():
-    return jsonify ({"status": "OK", "service": "SRE Project App"})
 
-@app.route('/error')
+@app.route("/status")
+def status():
+    return jsonify({"status": "OK", "service": "SRE Project App"})
+
+
+@app.route("/error")
 def error():
     response = make_response(jsonify({"error": "Internal Server Error"}), 500)
     return response
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,26 @@
 import sys
 import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from app import app
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 def test_home_page():
     client = app.test_client()
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
     assert b"Hello, World! Welcome to the SRE Project App." in response.data
 
+
 def test_status_page():
     client = app.test_client()
-    response = client.get('/status')
+    response = client.get("/status")
     assert response.status_code == 200
     json_data = response.get_json()
-    assert json_data['status'] == 'OK'
+    assert json_data["status"] == "OK"
+
 
 def test_error_page():
     client = app.test_client()
-    response = client.get('/error')
+    response = client.get("/error")
     assert response.status_code == 500
